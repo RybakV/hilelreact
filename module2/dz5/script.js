b9115af7fe0eb8c0e3f852382b3a51a480a9d65c
Viacheslav <viacheslav.rybak.pb@gmail.com>
2022-11-22 03:47:17 +0200

    DZ5 перездача

--- module2/dz5/script.js
+++ module2/dz5/script.js
@@ -1,15 +1,15 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr)
 
-switch (true) {
-    case (numOrStr === null):
+switch (numOrStr) {
+    case null:
         console.log('ви скасували')
         break;
-    case (numOrStr.trim() === ''):
+    case '':
         console.log('Empty String');
         break;
-    case (isNaN( +numOrStr )):
-        console.log(' number is Ba_NaN')
+    case (!isNaN(numOrStr) || numOrStr):
+        console.log('number is Ba_NaN')
         break;
     default:
         console.log('OK!')
