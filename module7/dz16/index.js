fed87aa7f94bd74f10058f051b76d45f23ebf0c6
Viacheslav <viacheslav.rybak.pb@gmail.com>
2023-01-14 23:39:44 +0200

    dz16 design update
    dz16 design update

--- module7/dz16/index.js
+++ module7/dz16/index.js
@@ -5,8 +5,8 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=
             .then(json => {
                   console.log(json);
                   weather.innerHTML += '<img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png" alt="' + json.weather.main + '"/>';
-                  weather.innerHTML += '<h4>Temperature: ' + json.main.temp + '°</h4>';
                   weather.innerHTML += '<h3>City: ' + json.name + '</h3>';
+                  weather.innerHTML += '<h4>Temperature: ' + json.main.temp + '°</h4>';
                   weather.innerHTML += '<p>Weather: ' + json.weather[0].main + ',' + json.weather[0].description + '</p>';
                   weather.innerHTML += '<p>Pressure: ' + json.main.pressure + '</p>';
                   weather.innerHTML += '<p>Humidity: ' + json.main.humidity + '</p>';
